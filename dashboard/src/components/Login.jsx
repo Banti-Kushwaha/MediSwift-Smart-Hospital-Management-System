8918900e846e4f002c16be1097d4b87bcec196c5
Banti Kushwaha <bantihzb987@gmail.com>
2026-04-20 18:10:17 +0530

    updated

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://mediswift-backend-fsux.onrender.com/api/v1/user/login",
         { email, password, confirmPassword, role: "Admin" },
         {
           withCredentials: true,
